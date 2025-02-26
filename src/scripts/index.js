dcaca0f280ff7cdccd242a292cf816f3d5473fd6
rotua1005 <rotuaekawati414@gmail.com>
2025-02-26 22:54:09 +0700

    web ypma progress

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+import 'regenerator-runtime'; // untuk async await transpile
+import '../styles/main.css';
+import '../styles/appBar.css';
+import '../styles/listData.css';
+import '../styles/footer.css';
+import data from '../public/data/DATA.json'; 
+
